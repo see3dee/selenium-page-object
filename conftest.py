8f3520543d8a82638b0cc5f2ef105e505fe8f22a
John Munsey <see3dee@gmail.com>
2021-01-03 16:53:53 -0800

    replace return with yield for browser teardown

--- conftest.py
+++ conftest.py
@@ -5,16 +5,16 @@ from selenium import webdriver
 @fixture(scope='function')
 def chrome_browser():
     browser = webdriver.Chrome()
-    return browser
+    yield browser
 
 
 @fixture(scope='function')
 def firefox_browser():
     browser = webdriver.Firefox()
-    return browser
+    yield browser
 
 
 @fixture(scope='function')
 def edge_browser():
     browser = webdriver.Edge()
-    return browser
+    yield browser
