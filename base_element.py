67ad6c922e4a2a656b19eba773a57bca1004d33d
John Munsey <see3dee@gmail.com>
2021-01-26 17:57:36 -0800

    more base element class/wrapper for selenium elements

--- base_element.py
+++ base_element.py
@@ -1,20 +1,37 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 '''
-Selenium web element is defined by the 
-BY (type of selector) and the Locator (string representation 
+Selenium locator is defined by the 
+BY (type of selector) and the value (string representation 
 of the type's path to the element)
 '''
 
+
 class BaseElement(object):
-    def __init__(self, driver, locator, by):
+    def __init__(self, driver, value, by):
         self.driver = driver
-        self.locator = locator
+        self.value = value
         self.by = by
         self.web_element = None
+        self.find()
 
     def find(self):
-        self.driver.find_element(by=self.by, value=self.locator)
+        locator = (self.by, self.value)
+        element = WebDriverWait(
+            self.driver, 10).until(
+            EC.visibility_of_element_located(locator=locator))
+        self.web_element = element
+        return None
+
+    def click(self):
+        locator = (self.by, self.value)
+        element = WebDriverWait(
+            self.driver, 10).until(
+            EC.element_to_be_clickable(locator=locator))
+
 
 
 
