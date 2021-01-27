3599d8d7cf8af421bd98d563aaa4cdc861f265bc
John Munsey <see3dee@gmail.com>
2021-01-26 16:30:52 -0800

    create methods before OO refactor

--- pom.py
+++ pom.py
@@ -1,17 +1,25 @@
+from selenium import webdriver
+
 
 class TrainingGroundPage:
-    def __init__(self):
-        pass
+    def __init__(self, driver):
+        self.driver = driver
 
     def type_into_input(self, text):
-        pass
+        inpt = self.driver.find_element_by_id('lpt1')
+        inpt.clear()
+        inpt.send_keys(text)
 
     def get_input_text(self):
-        pass
+        inpt = self.driver.find_element_by_id('lpt1')
+        elem_test = inpt.get_attrinute('value')
+        return elem_test
 
     def click_button_1(self):
-        pass
-    
+        button = self.driver.find_element_by_id('b1')
+        button.click()
+        return None
+
 
 
 
