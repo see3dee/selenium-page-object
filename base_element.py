4aa46a28556107894bbf59b81bc3ebc6f5efadb5
John Munsey <see3dee@gmail.com>
2021-01-26 18:01:48 -0800

    move locator into the class to reduce repeated lines

--- base_element.py
+++ base_element.py
@@ -15,22 +15,21 @@ class BaseElement(object):
         self.driver = driver
         self.value = value
         self.by = by
+        self.locator = (self.by, self.value)
         self.web_element = None
         self.find()
 
     def find(self):
-        locator = (self.by, self.value)
         element = WebDriverWait(
             self.driver, 10).until(
-            EC.visibility_of_element_located(locator=locator))
+            EC.visibility_of_element_located(locator=self.locator))
         self.web_element = element
         return None
 
     def click(self):
-        locator = (self.by, self.value)
         element = WebDriverWait(
             self.driver, 10).until(
-            EC.element_to_be_clickable(locator=locator))
+            EC.element_to_be_clickable(locator=self.locator))
 
 
 
